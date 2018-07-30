84c7674ebbf88c74c26c25853d1dc7a200d07243
omar aoutil <omaroutil@gmail.com>
2018-07-30 13:11:36 +0200

    chessboard table

---
+++ script.js
@@ -0,0 +1,56 @@
+
+var row = 0;
+while(row < 17){
+    var column = 0;
+    while(column < 17){
+        var square = document.createElement('div');
+        if(row % 2 === 0){
+            square.className = (column % 2 === 0) ? "white" : "black";
+        }else{
+            square.className = (column % 2 === 0) ? "black" : "white";
+        }
+  
+    if(column === 0){
+        square.className += " clear";
+    }
+    
+        document.body.appendChild(square);
+        column++;
+    
+    }
+
+    row++;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
